2c9a59464316dd134ca6560903037928e60c67c8
SHR-sky <1779558876@qq.com>
2024-03-13 20:45:37 +0800

    python创建csv数据集

--- 上位机工具/pyhton接收处理串口数据/Serial_get.py
+++ 上位机工具/pyhton接收处理串口数据/Serial_get.py
@@ -1,9 +1,30 @@
 import serial
+import csv
 
+# 打开串口
+ser = serial.Serial('COM13', 115200)  # 串口号和波特率
 
-com = serial.Serial('COM13',115200)
+# 创建 CSV 文件并写入数据
+with open('accelerometer_data.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Time', 'Acceleration_X', 'Acceleration_Y', 'Acceleration_Z'])  # 写入 CSV 文件的标题行
 
-while(1):
-    bit = com.read(1)
-    if com != r'\n':
-        print(bit)
+    try:
+        while True:
+            # 从串口读取数据
+            line = ser.readline().decode().strip()
+            
+            # 判断是否是加速度数据行
+            if line.startswith("aworld"):
+                data = line.split('\t')
+                timestamp = data[0]
+                accel_x = float(data[1])
+                accel_y = float(data[2])
+                accel_z = float(data[3])
+                
+                # 写入 CSV 文件
+                writer.writerow([timestamp, accel_x, accel_y, accel_z])
+                
+    except KeyboardInterrupt:
+        print("程序终止")
+        ser.close()
