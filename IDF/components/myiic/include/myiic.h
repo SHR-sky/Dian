634689d695ac9e06964fcb1fb74c1b256460d96e
SHR-sky <1779558876@qq.com>
2024-03-16 18:59:10 +0800

    进度更新

--- IDF/components/myiic/include/myiic.h
+++ IDF/components/myiic/include/myiic.h
@@ -4,7 +4,8 @@
 #include "driver/i2c.h"
 #include "esp_log.h"
 #include "esp_system.h"
-
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 
 #define DATA_LENGTH          512  /*!<Data buffer length for test buffer*/
@@ -14,15 +15,20 @@
 #define I2C_SLAVE_TX_BUF_LEN  (2*DATA_LENGTH) /*!<I2C slave tx buffer size */
 #define I2C_SLAVE_RX_BUF_LEN  (2*DATA_LENGTH) /*!<I2C slave rx buffer size */
 
-#define I2C_MASTER_SCL_IO     4     /*!<gpio number for i2c master clock  */
-#define I2C_MASTER_SDA_IO     5     /*!<gpio number for i2c master data  */
+#define I2C_MASTER_SCL_IO     7     /*!<gpio number for i2c master clock  */
+#define I2C_MASTER_SDA_IO     6     /*!<gpio number for i2c master data  */
 
 #define I2C_MASTER_NUM I2C_NUM_0   /*!< I2C port number for master dev */
 #define I2C_MASTER_TX_BUF_DISABLE   0   /*!< I2C master do not need buffer */
 #define I2C_MASTER_RX_BUF_DISABLE   0   /*!< I2C master do not need buffer */
 #define I2C_MASTER_FREQ_HZ    100000     /*!< I2C master clock frequency */
 
-#define ESP_SLAVE_ADDR 0x28         /*!< ESP32 slave address, you can set any 7bit value */
+
+//如果AD0脚(9脚)接地,IIC地址为0X68(不包含最低位).
+//如果接V3.3,则IIC地址为0X69(不包含最低位).
+
+#define MPU_ADDR 0x68             // MPU6050 设备地址
+
 #define WRITE_BIT  I2C_MASTER_WRITE /*!< I2C master write */
 #define READ_BIT   I2C_MASTER_READ  /*!< I2C master read */
 #define ACK_CHECK_EN   0x1     /*!< I2C master will check ack from slave*/
@@ -30,20 +36,83 @@
 #define ACK_VAL 0
 #define NACK_VAL 1
 
-
-
-
-
-
-#define MPU_ADDR 0x68             // MPU6050 设备地址
 #define MPU_CMD_WHO_AM_I 0x75     // MPU6050 设备确认寄存器
-#define MPU_CMD_PWR_MGMT_1 0x6B   // 电源管理寄存器
-#define MPU_CMD_SMPLRT_DIV 0x19   // 陀螺仪采样率分频器寄存器
-#define MPU_CMD_CONFIG 0x1A       // 数字低通滤波器配置寄存器
-#define MPU_CMD_GYRO_CONFIG 0x1B  // 陀螺仪配置寄存器
-#define MPU_CMD_ACCEL_CONFIG 0x1C // 加速度传感器配置寄存器
-#define MPU_CMD_ACCEL_XOUT_H 0x3B // 加速计X轴高字节数据寄存器
+
 static const char *TAG = "MPU6050";
+
+
+
+
+
+
+
+#define MPU_SELF_TESTX_REG		0X0D	//自检寄存器X
+#define MPU_SELF_TESTY_REG		0X0E	//自检寄存器Y
+#define MPU_SELF_TESTZ_REG		0X0F	//自检寄存器Z
+#define MPU_SELF_TESTA_REG		0X10	//自检寄存器A
+#define MPU_SAMPLE_RATE_REG		0X19	//采样频率分频器
+#define MPU_CFG_REG				0X1A	//配置寄存器
+#define MPU_GYRO_CFG_REG		0X1B	//陀螺仪配置寄存器
+#define MPU_ACCEL_CFG_REG		0X1C	//加速度计配置寄存器
+#define MPU_MOTION_DET_REG		0X1F	//运动检测阀值设置寄存器
+#define MPU_FIFO_EN_REG			0X23	//FIFO使能寄存器
+#define MPU_I2CMST_CTRL_REG		0X24	//IIC主机控制寄存器
+#define MPU_I2CSLV0_ADDR_REG	0X25	//IIC从机0器件地址寄存器
+#define MPU_I2CSLV0_REG			0X26	//IIC从机0数据地址寄存器
+#define MPU_I2CSLV0_CTRL_REG	0X27	//IIC从机0控制寄存器
+#define MPU_I2CSLV1_ADDR_REG	0X28	//IIC从机1器件地址寄存器
+#define MPU_I2CSLV1_REG			0X29	//IIC从机1数据地址寄存器
+#define MPU_I2CSLV1_CTRL_REG	0X2A	//IIC从机1控制寄存器
+#define MPU_I2CSLV2_ADDR_REG	0X2B	//IIC从机2器件地址寄存器
+#define MPU_I2CSLV2_REG			0X2C	//IIC从机2数据地址寄存器
+#define MPU_I2CSLV2_CTRL_REG	0X2D	//IIC从机2控制寄存器
+#define MPU_I2CSLV3_ADDR_REG	0X2E	//IIC从机3器件地址寄存器
+#define MPU_I2CSLV3_REG			0X2F	//IIC从机3数据地址寄存器
+#define MPU_I2CSLV3_CTRL_REG	0X30	//IIC从机3控制寄存器
+#define MPU_I2CSLV4_ADDR_REG	0X31	//IIC从机4器件地址寄存器
+#define MPU_I2CSLV4_REG			0X32	//IIC从机4数据地址寄存器
+#define MPU_I2CSLV4_DO_REG		0X33	//IIC从机4写数据寄存器
+#define MPU_I2CSLV4_CTRL_REG	0X34	//IIC从机4控制寄存器
+#define MPU_I2CSLV4_DI_REG		0X35	//IIC从机4读数据寄存器
+
+#define MPU_I2CMST_STA_REG		0X36	//IIC主机状态寄存器
+#define MPU_INTBP_CFG_REG		0X37	//中断/旁路设置寄存器
+#define MPU_INT_EN_REG			0X38	//中断使能寄存器
+#define MPU_INT_STA_REG			0X3A	//中断状态寄存器
+
+#define MPU_ACCEL_XOUTH_REG		0X3B	//加速度值,X轴高8位寄存器
+#define MPU_ACCEL_XOUTL_REG		0X3C	//加速度值,X轴低8位寄存器
+#define MPU_ACCEL_YOUTH_REG		0X3D	//加速度值,Y轴高8位寄存器
+#define MPU_ACCEL_YOUTL_REG		0X3E	//加速度值,Y轴低8位寄存器
+#define MPU_ACCEL_ZOUTH_REG		0X3F	//加速度值,Z轴高8位寄存器
+#define MPU_ACCEL_ZOUTL_REG		0X40	//加速度值,Z轴低8位寄存器
+
+#define MPU_TEMP_OUTH_REG		0X41	//温度值高八位寄存器
+#define MPU_TEMP_OUTL_REG		0X42	//温度值低8位寄存器
+
+#define MPU_GYRO_XOUTH_REG		0X43	//陀螺仪值,X轴高8位寄存器
+#define MPU_GYRO_XOUTL_REG		0X44	//陀螺仪值,X轴低8位寄存器
+#define MPU_GYRO_YOUTH_REG		0X45	//陀螺仪值,Y轴高8位寄存器
+#define MPU_GYRO_YOUTL_REG		0X46	//陀螺仪值,Y轴低8位寄存器
+#define MPU_GYRO_ZOUTH_REG		0X47	//陀螺仪值,Z轴高8位寄存器
+#define MPU_GYRO_ZOUTL_REG		0X48	//陀螺仪值,Z轴低8位寄存器
+
+#define MPU_I2CSLV0_DO_REG		0X63	//IIC从机0数据寄存器
+#define MPU_I2CSLV1_DO_REG		0X64	//IIC从机1数据寄存器
+#define MPU_I2CSLV2_DO_REG		0X65	//IIC从机2数据寄存器
+#define MPU_I2CSLV3_DO_REG		0X66	//IIC从机3数据寄存器
+
+#define MPU_I2CMST_DELAY_REG	0X67	//IIC主机延时管理寄存器
+#define MPU_SIGPATH_RST_REG		0X68	//信号通道复位寄存器
+#define MPU_MDETECT_CTRL_REG	0X69	//运动检测控制寄存器
+#define MPU_USER_CTRL_REG		0X6A	//用户控制寄存器
+#define MPU_PWR_MGMT1_REG		0X6B	//电源管理寄存器1
+#define MPU_PWR_MGMT2_REG		0X6C	//电源管理寄存器2
+#define MPU_FIFO_CNTH_REG		0X72	//FIFO计数寄存器高八位
+#define MPU_FIFO_CNTL_REG		0X73	//FIFO计数寄存器低八位
+#define MPU_FIFO_RW_REG			0X74	//FIFO读写寄存器
+#define MPU_DEVICE_ID_REG		0X75	//器件ID寄存器
+
 
 
 
